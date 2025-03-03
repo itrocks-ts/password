360c350bcaf21c03040f4c7728e7b941f8302129
Baptiste Pillot <baptiste@pillot.fr>
2025-03-03 16:01:42 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,9 @@
+[![npm version](https://img.shields.io/npm/v/@itrocks/password?logo=npm)](https://www.npmjs.org/package/@itrocks/password)
+[![npm downloads](https://img.shields.io/npm/dm/@itrocks/password)](https://www.npmjs.org/package/@itrocks/password)
+[![GitHub](https://img.shields.io/github/last-commit/itrocks-ts/password?color=2dba4e&label=commit&logo=github)](https://github.com/itrocks-ts/password)
+[![issues](https://img.shields.io/github/issues/itrocks-ts/password)](https://github.com/itrocks-ts/password/issues)
+[![discord](https://img.shields.io/discord/1314141024020467782?color=7289da&label=discord&logo=discord&logoColor=white)](https://25.re/ditr)
+
+# password
+
+@Password() decorator for secure formatting of password fields.
---
+++ src/password.ts
@@ -0,0 +1,33 @@
+import { KeyOf, ObjectOrType }   from '@itrocks/class-type'
+import { decorate, decoratorOf } from '@itrocks/decorator/property'
+
+const PASSWORD = Symbol('password')
+
+type Dependencies = {
+	setTransformers?: <T extends object>(target: T, property: KeyOf<T>) => void
+}
+
+const depends: Dependencies = {
+}
+
+export function passwordDependsOn(dependencies: Partial<Dependencies>)
+{
+	Object.assign(depends, dependencies)
+}
+
+export default Password
+export function Password<T extends object>(value = true)
+{
+	const parent = decorate<T>(PASSWORD, value)
+	return value
+		? (target: T, property: KeyOf<T>) => {
+			parent(target, property)
+			depends.setTransformers?.(target, property)
+		}
+		: parent
+}
+
+export function passwordOf<T extends object>(target: ObjectOrType<T>, property: KeyOf<T>)
+{
+	return decoratorOf(target, property, PASSWORD, false)
+}

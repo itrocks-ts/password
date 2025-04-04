a28c0712dde96bc118d92dc2dee1c3803453b34c
Baptiste Pillot <baptiste@pillot.fr>
2025-04-04 17:09:49 +0200

    Format HTML field id and name

--- src/transformers.ts
+++ src/transformers.ts
@@ -1,19 +1,22 @@
 import { KeyOf, ObjectOrType }     from '@itrocks/class-type'
 import { displayOf }               from '@itrocks/property-view'
+import { toCssId, toField }        from '@itrocks/rename'
 import { EDIT, HTML, IGNORE }      from '@itrocks/transformer'
 import { INPUT, JSON }             from '@itrocks/transformer'
 import { setPropertyTransformer }  from '@itrocks/transformer'
 import { setPropertyTransformers } from '@itrocks/transformer'
 import { tr }                      from '@itrocks/translate'
-import { createHash }              from 'crypto'
+import { createHash }              from 'node:crypto'
 
 const lfTab = '\n\t\t\t\t'
 
 export function editPassword<T extends object>(value: string, target: ObjectOrType<T>, property: KeyOf<T>)
 {
+	const fieldId    = toCssId(property)
+	const fieldName  = toField(property)
 	const inputValue = value.length ? ` value="${IGNORE}"` : ''
-	const label      = `<label for="${property}">${tr(displayOf(target, property))}</label>`
-	const input      = `<input id="${property}" name="${property}" type="password"${inputValue}>`
+	const label      = `<label for="${fieldId}">${tr(displayOf(target, property))}</label>`
+	const input      = `<input id="${fieldId}" name="${fieldName}" type="password"${inputValue}>`
 	return label + lfTab + input
 }
 
@@ -35,7 +38,7 @@ export function setPasswordHtmlTransformers<T extends object>(target: ObjectOrTy
 
 export function setPasswordJsonTransformers<T extends object>(target: ObjectOrType<T>, property: KeyOf<T>)
 {
-	setPropertyTransformer(target, property, JSON, '', (value: string) => value.length ? '*PASSWORD*' : '*EMPTY*')
+	setPropertyTransformer(target, property, JSON, '', (value: string) => (value === '') ? '*EMPTY*' : '*PASSWORD*')
 }
 
 export function setPasswordTransformers<T extends object>(target: ObjectOrType<T>, property: KeyOf<T>)
